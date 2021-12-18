bf50b0d44173264cab8040836c55280890bac5f8
Josh Buker <crypto@joshbuker.com>
2021-12-17 22:49:58 -0800

    Setting CORS to "*"? What could possibly go wrong.

--- index.js
+++ index.js
@@ -68,9 +68,16 @@ router.get("/:id", async ({ params }) => {
     },
   }
 
+  const options = {
+    headers: {
+      "content-type": "application/json;charset=UTF-8",
+      "Access-Control-Allow-Origin": "*",
+    },
+  }
+
   const response = await fetch(url, init)
   const results = await gatherResponse(response)
-  return new Response(results, init)
+  return new Response(results, options)
 })
 
 /*
